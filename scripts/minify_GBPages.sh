20c71fa5b6b853e8f8ae9c41f71fbac9664d6b21
BooterMan <cesarpaulangelo@gmail.com>
2020-12-27 03:10:08 -0300

    Now it doesn't compress when publishing to GH Pages

---
+++ scripts/minify_GBPages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# based on:
+# https://unix.stackexchange.com/questions/249701/how-to-minify-javascript-and-css-with-command-line-using-minify-tool
+# minification of js files
+
+./scripts/minify_dev.sh
+npx terser js/init.js -c -m -o js/init.js
+find js/ -regex '.*[^(?:min[0-9+)|(init)]\.js' \
+  -delete
+
+# minification of css files
+find css/ -type f \
+    -name "*.css" ! -name "*.min.*" \
+    -exec echo {} \; \
+    -exec npx uglifycss --output {}.min {} \; \
+    -exec rm {} \; \
+    -exec mv -f {}.min {} \;
